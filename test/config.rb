8b10dda7a2069ebf17398d30deb8f16e8bd461b3
Ari Russo <ari.russo@gmail.com>
2011-05-07 21:03:38 -0400

    tests

--- test/config.rb
+++ test/config.rb
@@ -8,4 +8,8 @@ module TestHelper::Config
   TestInput = UniMIDI::Input.first # this is the device you wish to use to test input
   TestOutput = UniMIDI::Output.first # likewise for output
 
+  puts "connect #{TestInput.name} (#{TestInput.id}) to #{TestOutput.name} (#{TestOutput.id}) and press enter"
+  $stdin.gets.chomp
+
 end
+
