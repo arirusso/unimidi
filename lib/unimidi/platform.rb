957fa225a711befe9dc5657c878cd3d823d44246
Ari Russo <ari.russo@gmail.com>
2011-05-09 21:12:04 -0400

    indentation

--- lib/unimidi/platform.rb
+++ lib/unimidi/platform.rb
@@ -4,30 +4,30 @@
 require 'singleton'
 
 module UniMIDI
-    
-    class Platform
-      
-      include Singleton
+  
+  class Platform
 
-      attr_reader :interface
-      
-      def initialize
-        lib = case RUBY_PLATFORM
-          when /darwin/ then "ffi-coremidi"
-          when /java/ then "midi-jruby"
-          when /linux/ then "alsa-rawmidi"
-          when /mingw/ then "midi-winmm"
-          when /win/ then "midi-winmm"
-        end
-        require("unimidi/adapter/#{lib}")
-        @interface = case RUBY_PLATFORM
-          when /darwin/ then CoreMIDIAdapter
-          when /java/ then MIDIJRubyAdapter
-          when /linux/ then AlsaRawMIDIAdapter
-          when /mingw/ then MIDIWinMMAdapter
-          when /win/ then MIDIWinMMAdapter 
-        end
+    include Singleton
+
+    attr_reader :interface
+    
+    def initialize
+      lib = case RUBY_PLATFORM
+        when /darwin/ then "ffi-coremidi"
+        when /java/ then "midi-jruby"
+        when /linux/ then "alsa-rawmidi"
+        when /mingw/ then "midi-winmm"
+        when /win/ then "midi-winmm"
       end
+      require("unimidi/adapter/#{lib}")
+      @interface = case RUBY_PLATFORM
+        when /darwin/ then CoreMIDIAdapter
+        when /java/ then MIDIJRubyAdapter
+        when /linux/ then AlsaRawMIDIAdapter
+        when /mingw/ then MIDIWinMMAdapter
+        when /win/ then MIDIWinMMAdapter
+      end
+    end
 
   end
 
