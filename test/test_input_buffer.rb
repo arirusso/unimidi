1de7df5493c47248a42663578998d210b301def8
Ari Russo <ari.russo@gmail.com>
2011-05-10 01:19:11 -0400

    input buffer test

--- test/test_input_buffer.rb
+++ test/test_input_buffer.rb
@@ -8,6 +8,39 @@ class InputBufferTest < Test::Unit::TestCase
   include TestHelper
   include TestHelper::Config # before running these tests, adjust the constants in config.rb to suit your hardware setup
   
+  def test_input_buffer
   
+    sleep(1)
+
+    messages = VariousMIDIMessages
+    bytes = []
+
+    TestOutput.open do |output|
+      TestInput.open do |input|
+
+        messages.each do |msg|
+
+          $>.puts "sending: " + msg.inspect
+
+          output.puts(msg)
+          
+          bytes += msg 
+          
+          sleep(0.5)
+          
+          buffer = input.buffer.map { |m| m[:data] }.flatten
+
+          $>.puts "received: " + buffer.to_s
+          
+          assert_equal(bytes, buffer)
+
+        end
+        
+        assert_equal(input.buffer.length, messages.length)
+
+      end
+    end
+    
+  end
 
 end
