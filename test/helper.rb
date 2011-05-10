4b3b10c96a548c6567ed7d86555705b485ced4ce
Ari Russo <ari.russo@gmail.com>
2011-05-09 20:08:45 -0400

    remove sysex test for jruby (see readme)

--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,8 @@ require 'unimidi'
 
 module TestHelper
   
+  TestSysex = true unless RUBY_PLATFORM.include?("java")
+  
   def platform_test(adapter, mod)
     assert_equal(adapter, UniMIDI::Platform.instance.interface)
     assert_not_same(mod::Input, UniMIDI::Input)
@@ -31,20 +33,20 @@ module TestHelper
     
   # some MIDI messages
   VariousMIDIMessages = [
-    [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7], # SysEx
+    TestSysex ? [0xF0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7F, 0x00, 0x41, 0xF7] : nil, # SysEx
     [0x90, 100, 100], # note on
     [0x90, 43, 100], # note on
     [0x90, 76, 100], # note on
     [0x90, 60, 100], # note on
     [0x80, 100, 100] # note off
-  ]
+  ].compact
     
   # some MIDI messages
   VariousMIDIByteStrMessages = [
-    "F04110421240007F0041F7", # SysEx
+    TestSysex ? "F04110421240007F0041F7" : nil, # SysEx
     "906440", # note on
     "804340" # note off
-  ]
+  ].compact
     
 end
 
