229c9ac556f919b7ba2f3f392874f091b0485445
Ari Russo <ari.russo@gmail.com>
2013-06-08 17:21:42 -0400

    explanation of module

--- lib/unimidi/congruous_api_adapter.rb
+++ lib/unimidi/congruous_api_adapter.rb
@@ -1,5 +1,6 @@
-# Module that allows easy delegation to underlying MIDI libraries where the underlying library fits the format 
-# specified by the alsa-rawmidi, ffi-coremidi, midi-jruby and midi-winmm gems.
+# Module that allows delegation to underlying MIDI libraries where the underlying library fits the format 
+# specified by the alsa-rawmidi, ffi-coremidi, midi-jruby and midi-winmm gems.  The interface of these libraries
+# are similar, but not similar enough to use simple delegation.
 module CongruousApiAdapter
 end
 
