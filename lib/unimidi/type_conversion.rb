fa1fddb381c7d7dcac81b6aee1dca5bea5b719b3
Ari Russo <ari.russo@gmail.com>
2013-06-08 12:39:35 -0400

    type conversion method was broken, better comments

--- lib/unimidi/type_conversion.rb
+++ lib/unimidi/type_conversion.rb
@@ -1,10 +1,15 @@
 module UniMIDI
   
+  # Utility module for converting between different data formats
   module TypeConversion
+
+    extend self
       
-    # Convert a byte array to string of hex bytes 
+    # Convert an array of numeric bytes to string of hex bytes 
+    # @param [Array<Fixnum>] byte An array of numeric bytes eg [0x90, 0x40, 0x40]
+    # @return [String] A string of hex bytes eg "904040"
     def numeric_byte_array_to_hex_string(bytes)
-      bytes.map { |b| b.hex }.join
+      bytes.map { |b| b.to_s(16) }.join
     end
       
   end
