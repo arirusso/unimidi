67d5dee4f50b3589d5bb8f2ce8607141d4fad069
Ari Russo <ari.russo@gmail.com>
2013-06-08 17:49:32 -0400

    chain modules

--- lib/unimidi/congruous_api_adapter/device_methods.rb
+++ lib/unimidi/congruous_api_adapter/device_methods.rb
@@ -5,6 +5,10 @@ module UniMIDI
     # Class methods used by the outer device module adapter
     module DeviceMethods
 
+      def self.extended(base)
+        base.send(:extend, CongruousApiAdapter::ClassMethods)
+      end
+
       # An array of all devices
       def all
         all_by_type.values.flatten
