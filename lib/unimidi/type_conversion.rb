d041a377e85e5ea184bf6ca982e83604fdf0da32
Ari Russo <ari.russo@gmail.com>
2013-06-08 12:24:18 -0400

    no need for shebang, better comment

--- lib/unimidi/type_conversion.rb
+++ lib/unimidi/type_conversion.rb
@@ -1,11 +1,8 @@
-#!/usr/bin/env ruby
-#
-
 module UniMIDI
   
   module TypeConversion
       
-    # byte array to string of hex bytes 
+    # Convert a byte array to string of hex bytes 
     def numeric_byte_array_to_hex_string(bytes)
       bytes.map { |b| b.hex }.join
     end
