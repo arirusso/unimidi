e47708b7995a371b51f26a3ce8eadb3d7532a053
Ari Russo <ari.russo@gmail.com>
2013-06-08 12:24:00 -0400

    no need for shebang, organize requires

--- lib/unimidi.rb
+++ lib/unimidi.rb
@@ -1,20 +1,21 @@
-#!/usr/bin/env ruby
-#
 # A realtime MIDI interface for Ruby
-# (c)2010-2011 Ari Russo and licensed under the Apache 2.0 License
-#
-
+# (c)2010-2013 Ari Russo and licensed under the Apache 2.0 License
 module UniMIDI
   
   VERSION = "0.3.3"
  
 end
 
-require 'forwardable'
+# libs
+require "forwardable"
+require "singleton"
 
-require 'unimidi/congruous_api_adapter'
-require 'unimidi/platform'
-require 'unimidi/type_conversion'
+# modules
+require "unimidi/congruous_api_adapter"
+require "unimidi/type_conversion"
+
+# classes
+require "unimidi/platform"
 
 module UniMIDI
   extend(Platform.instance.interface)
