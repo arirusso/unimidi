5e6fc041e5eec2c825d9366075c085b497de3e1e
Ari Russo <ari.russo@gmail.com>
2014-03-28 19:56:59 -0400

    include mocha and shoulda

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,10 @@
 dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + '/../lib'
 
-require 'test/unit'
-require 'unimidi'
+require "test/unit"
+require "mocha/test_unit"
+require "shoulda-context"
+require "unimidi"
 
 module UniMIDI
 
