fa5c6c630632701d822d7ab4d7003045879f357b
Ari Russo <ari.russo@gmail.com>
2014-08-30 23:03:20 -0400

    better name, comments

--- lib/unimidi/platform.rb
+++ lib/unimidi/platform.rb
@@ -1,10 +1,12 @@
 module UniMIDI
-  
+
+  # Deal with different dependencies between different user environments
   module Platform
 
     extend self
     
-    def init
+    # Loads the proper MIDI library and adapter for the user's environment
+    def bootstrap
       lib = case RUBY_PLATFORM
         when /darwin/ then "ffi-coremidi"
         when /java/ then "midi-jruby"
