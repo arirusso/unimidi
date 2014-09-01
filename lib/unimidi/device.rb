682e8790495e9384c4726aafed6a35b29f13c4fa
Ari Russo <ari.russo@gmail.com>
2014-09-01 13:25:54 -0400

    comments, readability

--- lib/unimidi/device.rb
+++ lib/unimidi/device.rb
@@ -8,12 +8,13 @@ module UniMIDI
 
       include Enumerable
 
-      # Iterate over all devices of this type
+      # Iterate over all devices of this direction (eg Input, Output)
       def each(&block)
         all.each { |device| yield(device) }
       end
 
       # Prints ids and names of each device to the console
+      # @return [Array<String>]
       def list
         all.map do |device| 
           name = device.pretty_name
@@ -22,12 +23,15 @@ module UniMIDI
         end
       end
 
-      # Shortcut to get a device by its name
+      # Shortcut to select a device by its name
+      # @param [String, Symbol] name
+      # @return [Input, Output]
       def find_by_name(name)
-        all.find { |device| name == device.name }
+        all.find { |device| name.to_s == device.name }
       end
 
-      # streamlined console prompt that asks the user to select a device
+      # Streamlined console prompt that asks the user to select a device
+      # When their input is received, the device is selected and enabled
       def gets(&block)
         device = nil
         class_name = self.name.split("::").last.downcase
@@ -43,19 +47,24 @@ module UniMIDI
           end
         end
         device.open(&block) unless device.enabled?
+        device
       end
 
-      # returns the first device for this class
+      # Select the first device and enable it
+      # @return [Input, Output]
       def first(&block)
         use_device(all.first, &block)
       end
 
-      # returns the last device for this class
+      # Select the last device and enable it
+      # @return [Input, Output]
       def last(&block)
         use_device(all.last, &block)
       end
 
-      # returns the device at <em>index</em> and opens it
+      # Select the device at the given index and enable it
+      # @param [Fixnum] index
+      # @return [Input, Output]
       def use(index, &block)
         index = case index
                 when :first then 0
@@ -66,13 +75,18 @@ module UniMIDI
       end
       alias_method :open, :use
 
-      # returns the device at <em>index</em>
+      # Select the device at the given index
+      # @param [Fixnum] index
+      # @return [Input, Output]
       def [](index)
         all[index]
       end
 
       private
 
+      # Enable the given device
+      # @param [Input, Output] device
+      # @return [Input, Output]
       def use_device(device, &block)
         device.open(&block) unless device.enabled?
         device         
@@ -83,22 +97,28 @@ module UniMIDI
     # Methods that are shared by both Input and Output instances
     module InstanceMethods
 
-      def initialize(device_obj)        
-        @device = device_obj
-        @id = @device.id
-        @name = @device.name
+      # @param [AlsaRawMIDI::Input, AlsaRawMIDI::Output, CoreMIDI::Destination, CoreMIDI::Source, MIDIJRuby::Input, MIDIJRuby::Output, MIDIWinMM::Input, MIDIWinMM::Output] device
+      def initialize(device)        
+        @device = device
         @enabled = false
-        populate_type
+
+        populate_from_device
       end
 
+      # Is this device ready for io?
+      # @return [Boolean]
       def enabled?
         @enabled = true if @device.enabled # keep the adapter in sync
         @enabled
       end
 
-      # enable the device for use, can be passed a block to which the device will be passed back
-      def open(*a, &block)
-        @device.open(*a) unless enabled?
+      # Enable the device for use
+      # Params are passed to the underlying device object
+      # Can be passed a block to which the device will be passed in as the yieldparam
+      # @param [*Object] args
+      # @return [Input, Output] self
+      def open(*args, &block)
+        @device.open(*args) unless enabled?
         @enabled = true
         if block_given?
           begin
@@ -114,29 +134,45 @@ module UniMIDI
         end
       end
 
+      # A human readable display name for this device
+      # @return [String]
       def pretty_name
         "#{id}) #{name}"
       end
 
-      # close the device
-      def close(*a)
-        @device.close(*a)
+      # Close the device
+      # Params are passed to the underlying device object
+      # @param [*Object] args
+      # @return [Boolean]
+      def close(*args)
+        @device.close(*args)
+        true
       end
 
+      # Add attributes for the device instance
+      # :direction, :id, :name
       def self.included(base)
-        base.send(:attr_reader, :name)
+        base.send(:attr_reader, :direction)
         base.send(:attr_reader, :id)
-        base.send(:attr_reader, :type)
-        base.send(:alias_method, :direction, :type)
+        base.send(:attr_reader, :name)
+        base.send(:alias_method, :type, :direction)
       end
 
       private
 
-      def populate_type
-        @type = case @device.type
+      # Populate the direction attribute
+      def populate_direction
+        @direction = case @device.type
                 when :source, :input then :input
                 when :destination, :output then :output
                 end
+      end
+
+      # Populate attributes from the underlying device object
+      def populate_from_device
+        @id = @device.id
+        @name = @device.name
+        populate_direction
       end
 
     end
