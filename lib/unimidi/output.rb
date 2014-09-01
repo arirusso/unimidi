a7c6a88e78d795d7d8b35d1aff8ccc1f668bf265
Ari Russo <ari.russo@gmail.com>
2014-09-01 14:09:49 -0400

    comments, readability

--- lib/unimidi/output.rb
+++ lib/unimidi/output.rb
@@ -1,48 +1,61 @@
 module UniMIDI
 
+  # A MIDI output device
   class Output
 
     extend Device::ClassMethods
     include Device::InstanceMethods
 
+    # All MIDI output devices -- used to populate the class
+    # @return [Array<Output>]
     def self.all
       Loader.devices(:type => :output)
     end
 
-    # sends a message to the output. the message can be:
+    # Sends a message to the output. 
     #
-    # bytes eg output.puts(0x90, 0x40, 0x40)
-    # an array of bytes eg output.puts([0x90, 0x40, 0x40])
-    # or a string eg output.puts("904040")
-    # if none of those types are found, unimidi will attempt 
-    # to call to_bytes and then to_a on the object
+    # The message format can be:
     #
-    def puts(*a)
-      case a.first
-      when Array then puts_bytes(*a.first)
-      when Numeric then puts_bytes(*a)
-      when String then puts_s(*a)
+    # 1. Numeric bytes eg output.puts(0x90, 0x40, 0x40)
+    # 2. An array of numeric bytes [0x90, 0x40, 0x40]
+    # 3. A string of bytes eg "904040"
+    # 4. An array of strings ["904040", "804040"]
+    #
+    # @param [*Array<Fixnum>, *Array<String>, *Fixnum, *String] messages
+    # @return [Array<Fixnum>, Array<String>]
+    def puts(*messages)
+      message = messages.first
+      case message
+      when Array then message.each { |message| puts(*message) }
+      when Fixnum then puts_bytes(*messages)
+      when String then puts_s(*messages)
       else
-        if a.first.respond_to?(:to_bytes)
-          puts_bytes(*a.first.to_bytes.flatten)
-        elsif a.first.respond_to?(:to_a) 
-          puts_bytes(*a.first.to_a.flatten)
+        if message.respond_to?(:to_bytes)
+          puts_bytes(*message.to_bytes.flatten)
+        elsif message.respond_to?(:to_a) 
+          puts_bytes(*message.to_a.flatten)
         end
       end
     end
 
-    # sends a message to the output in a form of a string eg "904040".  this method does not do
-    # type checking and therefore is more performant than puts
-    def puts_s(*a)
-      @device.puts_s(*a)
+    # Sends a message to the output in a form of a string eg "904040".  This method does not do
+    # type checking
+    # @param [*String] messages
+    # @return [Array<String>, Array<Array<String>>]
+    def puts_s(*messages)
+      @device.puts_s(*messages)
+      messages.count < 2 ? messages[0] : messages
     end
     alias_method :puts_bytestr, :puts_s
     alias_method :puts_hex, :puts_s
 
-    # sends a message to the output in a form of bytes eg output.puts_bytes(0x90, 0x40, 0x40).
-    # this method does not do type checking and therefore is more performant than puts
-    def puts_bytes(*a)
-      @device.puts_bytes(*a)
+    # Sends a message to the output in a form of bytes eg output.puts_bytes(0x90, 0x40, 0x40).
+    # This method does not do type checking.
+    # @param [*Array<Fixnum>] messages
+    # @return [Array<Fixnum>, Array<Array<Fixnum>>]
+    def puts_bytes(*messages)
+      @device.puts_bytes(*messages)
+      messages.count < 2 ? messages[0] : messages
     end
 
   end
