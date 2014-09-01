be4d79b5cdf2b70e8b10d258e920004a443e1b9d
Ari Russo <ari.russo@gmail.com>
2014-09-01 13:36:12 -0400

    comments, readability

--- lib/unimidi/input.rb
+++ lib/unimidi/input.rb
@@ -1,45 +1,56 @@
 module UniMIDI
 
+  # A MIDI input device
   class Input
 
     extend Device::ClassMethods
     include Device::InstanceMethods
 
+    # All MIDI input devices -- used to populate the class
+    # @return [Array<Input>]
     def self.all
       Loader.devices(:type => :input)
     end
 
+    # The device buffer
+    # @return [Array<Hash>]
     def buffer
       @device.buffer
     end
 
     #
-    # returns an array of MIDI event hashes as such:
+    # Plucks data from the input buffer and returns it as array of MIDI event hashes as such:
     #   [
     #     { :data => [144, 60, 100], :timestamp => 1024 },
     #     { :data => [128, 60, 100], :timestamp => 1100 },
     #     { :data => [144, 40, 120], :timestamp => 1200 }
     #   ]
     #
-    # the data is an array of Numeric bytes
-    # the timestamp is the number of millis since this input was enabled
+    # In this case, the data is an array of Numeric bytes
+    # The timestamp is the number of millis since this input was enabled
+    # Arguments are passed to the underlying device object
     #
-    def gets(*a)
-      @device.gets(*a)
+    # @param [*Object] args
+    # @return [Array<Hash>]
+    def gets(*args)
+      @device.gets(*args)
     rescue SystemExit, Interrupt
       exit
     end
 
     #
-    # same as gets but returns message data as string of hex digits as such:
+    # Plucks data from the input buffer and returns it as array of MIDI event hashes.
+    # Similar to Input#gets except that the returned message data as string of hex digits eg:
     #   [
     #     { :data => "904060", :timestamp => 904 },
     #     { :data => "804060", :timestamp => 1150 },
     #     { :data => "90447F", :timestamp => 1300 }
     #   ]
     #
-    def gets_s(*a)
-      @device.gets_s(*a)
+    # @param [*Object] args
+    # @return [Array<Hash>]
+    def gets_s(*args)
+      @device.gets_s(*args)
     rescue SystemExit, Interrupt
       exit
     end
@@ -47,42 +58,53 @@ module UniMIDI
     alias_method :gets_hex, :gets_s
 
     #
-    # returns an array of data bytes such as
+    # Plucks data from the input buffer and returns it as an array of data bytes such as
     #   [144, 60, 100, 128, 60, 100, 144, 40, 120]
     #
-    def gets_data(*a)
-      arr = gets
+    # @param [*Object] args
+    # @return [Array<Fixnum>]
+    def gets_data(*args)
+      arr = gets(*args)
       arr.map { |msg| msg[:data] }.inject(:+)
     end
 
     #
-    # returns a string of data such as
+    # Plucks data from the input buffer and returns it as a string of data such as
     #   "90406080406090447F"
     #
-    def gets_data_s(*a)
-      arr = gets_bytestr
+    # @param [*Object] args
+    # @return [String]
+    def gets_data_s(*args)
+      arr = gets_bytestr(*args)
       arr.map { |msg| msg[:data] }.join
     end
     alias_method :gets_data_bytestr, :gets_data_s
     alias_method :gets_data_hex, :gets_data_s
 
-    # clears the buffer
+    # Clears the input buffer
+    # @return [Array]
     def clear_buffer
       @device.buffer.clear
     end
 
-    # gets any messages in the buffer in the same format as Input#gets 
-    def gets_buffer(*a)
+    # Gets any messages in the buffer in the same format as Input#gets, without removing them from the buffer
+    # @param [*Object] args
+    # @return [Array<Hash>]
+    def gets_buffer(*args)
       @device.buffer
     end
 
-    # gets any messages in the buffer in the same format as Input#gets_s
-    def gets_buffer_s(*a)
+    # Gets any messages in the buffer in the same format as Input#gets_s, without removing them from the buffer
+    # @param [*Object] args
+    # @return [Array<Hash>]
+    def gets_buffer_s(*args)
       @device.buffer.map { |msg| msg[:data] = TypeConversion.numeric_byte_array_to_hex_string(msg[:data]); msg }
     end
 
-    # gets any messages in the buffer in the same format as Input#gets_data
-    def gets_buffer_data(*a)
+    # Gets any messages in the buffer in the same format as Input#gets_data without removing them from the buffer
+    # @param [*Object] args
+    # @return [Array<Fixnum>]
+    def gets_buffer_data(*args)
       @device.buffer.map { |msg| msg[:data] }
     end
 
