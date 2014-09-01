4730025665972bc7795b3e6b2ae29ea8939b5df9
Ari Russo <ari.russo@gmail.com>
2014-09-01 14:27:10 -0400

    comments

--- lib/unimidi/adapter/ffi-coremidi.rb
+++ lib/unimidi/adapter/ffi-coremidi.rb
@@ -4,16 +4,19 @@ module UniMIDI
 
   module Adapter
 
+    # Load underlying devices using the coremidi gem
     module CoreMIDI
 
       module Loader
 
         extend self
 
+        # @return [Array<CoreMIDI::Source>]
         def inputs
           ::CoreMIDI::Endpoint.all_by_type[:source]
         end
 
+        # @return [Array<CoreMIDI::Destination>]
         def outputs
           ::CoreMIDI::Endpoint.all_by_type[:destination]
         end
--- lib/unimidi/adapter/midi-winmm.rb
+++ lib/unimidi/adapter/midi-winmm.rb
@@ -4,16 +4,19 @@ module UniMIDI
 
   module Adapter
 
+    # Load underlying devices using the midi-winmm gem
     module MIDIWinMM
 
       module Loader
 
         extend self
 
+        # @return [Array<MIDIWinMM::Input>]
         def inputs
           ::MIDIWinMM::Device.all_by_type[:input]
         end
 
+        # @return [Array<MIDIWinMM::Output>]
         def outputs
           ::MIDIWinMM::Device.all_by_type[:output]
         end
