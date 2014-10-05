aae02802ca552197e49237c31434a08847549de5
David Bishop <david@gnuconsulting.com>
2014-10-04 21:47:41 -0400

    Specify minitest 4.7.5 as 5.x is incompatible

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,7 @@
 dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + '/../lib'
 
+gem "minitest", "4.7.5"
 require "test/unit"
 require "mocha/test_unit"
 require "shoulda-context"
