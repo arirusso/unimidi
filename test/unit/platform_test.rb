2f244cab7aafd8cf63dd2987693828de6516f6ab
Ari Russo <ari.russo@gmail.com>
2016-09-18 14:38:40 -0400

    add method context

--- test/unit/platform_test.rb
+++ test/unit/platform_test.rb
@@ -4,28 +4,32 @@ class UniMIDI::PlatformTest < Minitest::Test
 
   context "Platform" do
 
-    if RUBY_PLATFORM.include?("java")
-      should "recognize java" do
-        assert_equal(UniMIDI::Adapter::MIDIJRuby::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
-      end
-    end
+    context ".bootstrap" do
 
-    if RUBY_PLATFORM.include?("linux")
-      should "recognize linux" do
-        assert_equal(UniMIDI::Adapter::AlsaRawMIDI::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+      if RUBY_PLATFORM.include?("java")
+        should "recognize java" do
+          assert_equal(UniMIDI::Adapter::MIDIJRuby::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+        end
       end
-    end
 
-    if RUBY_PLATFORM.include?("darwin")
-      should "recognize osx" do
-        assert_equal(UniMIDI::Adapter::CoreMIDI::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+      if RUBY_PLATFORM.include?("linux")
+        should "recognize linux" do
+          assert_equal(UniMIDI::Adapter::AlsaRawMIDI::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+        end
       end
-    end
 
-    if RUBY_PLATFORM.include?("mingw")
-      should "recognize windows" do
-        assert_equal(UniMIDI::Adapter::MIDIWinMM::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+      if RUBY_PLATFORM.include?("darwin")
+        should "recognize osx" do
+          assert_equal(UniMIDI::Adapter::CoreMIDI::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+        end
       end
+
+      if RUBY_PLATFORM.include?("mingw")
+        should "recognize windows" do
+          assert_equal(UniMIDI::Adapter::MIDIWinMM::Loader, UniMIDI::Loader.instance_variable_get("@loader"))
+        end
+      end
+
     end
 
   end
